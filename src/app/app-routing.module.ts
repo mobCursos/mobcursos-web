0fcaf62af0135fe8cd9e7c91b7ec4fb11ad15e3d
Rodrigo Palermo <avilapalermo@gmail.com>
2020-11-14 00:02:18 -0300

    Adapt nav links to routerLink

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { UsersComponent } from './users/users.component';
 
-
+const routes: Routes = [
+  { path: 'users', component: UsersComponent },
+]
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
