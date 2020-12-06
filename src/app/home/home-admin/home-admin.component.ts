34463b81181700a279489c320b20d66c6f0b2d40
Rodrigo Palermo <avilapalermo@gmail.com>
2020-12-06 00:26:13 -0300

    Add counts for admin home

--- src/app/home/home-admin/home-admin.component.ts
+++ src/app/home/home-admin/home-admin.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+import { CourseService } from 'src/app/courses/course.service';
+import { Course } from 'src/app/courses/interfaces/course';
+import { User } from 'src/app/users/interfaces/user';
+import { UserService } from 'src/app/users/user.service';
 
 @Component({
   selector: 'app-home-admin',
@@ -7,9 +13,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeAdminComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    public userService: UserService,
+    public courseService: CourseService,
+  ) { }
+
+  totalUsers: number;
+  totalUsersAdmin: number;
+  totalUsersTeacher: number;
+  totalUsersStudent: number;
+  totalCourses: number;
+  users: User[];
+  courses: Course[];
 
   ngOnInit(): void {
+    this.userService.getUsers().subscribe(
+      users => {
+        this.totalUsers = users.length;
+        this.totalUsersAdmin = users.filter(user => user.role == 'admin').length;
+        this.totalUsersTeacher = users.filter(user => user.role == 'teacher').length;
+        this.totalUsersStudent = users.filter(user => user.role == 'student').length;
+      }
+    );
+    
+    this.courseService.getCourses(true).subscribe(
+      courses => this.totalCourses = courses.length
+    );
+    
   }
 
 }
