fc3bd47a885b8b8e501bb3e80f99ca0c56b6318f
Rodrigo Palermo <avilapalermo@gmail.com>
2020-12-09 16:41:26 -0300

    Add server file to start on heroku

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+
+app.use(express.static('./dist/mobcursos-web'));
+
+app.get('/*', (req, res) =>
+res.sendFile('index.html', {root: 'dist/mobcursos-web/'})
+);
+
+app.listen(process.env.PORT || 8080);
+
+console.log('ANGULAR server running');
