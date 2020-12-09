181d7a49957c5bdbb27f1b87a6f45930400f1343
Rodrigo Palermo <avilapalermo@gmail.com>
2020-12-09 02:33:04 -0300

    Add cors to server.js

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
 
+app.use(cors);
 app.use(express.static('./dist/mobcursos-web'));
 
 app.get('/*', (req, res) =>
